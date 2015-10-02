ae4487b4e7ea6e9c2a18c13e2ec30eb4ce2c1d32
James Edward Gray II <james@graysoftinc.com>
2015-10-02 10:30:50 -0500

    A different type of storage.

---
+++ array/lib/minesweeper/grid.rb
@@ -0,0 +1,48 @@
+module Minesweeper
+  class Grid
+    def initialize(cells)
+      @width  = cells.first.size
+      @height = cells.size
+      @cells  = cells.flatten
+    end
+
+    attr_reader :width, :height
+
+    attr_reader :cells
+    private     :cells
+
+    def [](x, y)
+      cells[y * width + x]
+    end
+
+    def neighbors(x, y)
+      [ [-1, -1], [ 0, -1], [ 1, -1],
+        [-1,  0],           [ 1,  0],
+        [-1,  1], [ 0,  1], [ 1,  1] ].map { |x_offset, y_offset|
+        neighbor_x, neighbor_y = x + x_offset, y + y_offset
+        if neighbor_x.between?(0, width  - 1) &&
+           neighbor_y.between?(0, height - 1)
+          self[neighbor_x, neighbor_y]
+        else
+          nil
+        end
+      }.compact
+    end
+
+    def count_bombs_around(x, y)
+      neighbors(x, y).count { |cell| cell == "X" }
+    end
+
+    def count_all
+      Array.new(height) { |y|
+        Array.new(width) { |x|
+          if self[x, y] == "X"
+            -1
+          else
+            count_bombs_around(x, y)
+          end
+        }
+      }
+    end
+  end
+end
